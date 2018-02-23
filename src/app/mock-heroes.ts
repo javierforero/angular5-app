296fd6c1f4e86a48295b352ad7fa8177226d9e55
Javier Forero <javierforero01@gmail.com>
2018-02-23 16:24:25 -0600

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 2, name: 'Hamlet'},
+  {id: 3, name: 'Javier'},
+  {id: 4, name: 'Cam'},
+  {id: 5, name: 'John'},
+  {id: 6, name: 'Leslie'},
+  {id: 7, name: 'Carrie'},
+  {id: 8, name: 'Maria'},
+  {id: 9, name: 'Joan'},
+  {id: 10, name: 'Jefe'},
+  {id: 11, name: 'Papi'}
+];
